eb0e860101c9e7608ca89101ea92aa2842b3edc5
Nikolay Borzov <nickborzoff@gmail.com>
2017-05-20 19:43:42 +0400

    Release 1.3.1

---
+++ Gulpfile.js
@@ -0,0 +1,20 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+var replace = require('gulp-replace');
+var rename = require("gulp-rename");
+
+gulp.task('build-css', function() {
+  gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(replace('/*POSTSASS ', ''))
+    .pipe(replace(' POSTSASS*/', ''))
+    .pipe(replace(/\nSTUB/g, ''))
+    .pipe(rename({
+      extname: '.qss'
+    }))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('default', function() {
+  gulp.watch('sass/**/*.scss', ['build-css']);
+});
